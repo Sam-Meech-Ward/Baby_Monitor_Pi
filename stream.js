25e6241d8e9dc0cf3f60fb9e85e15fd0f559f25b
Sam Meech-Ward <sam@meech-ward.com>
2018-07-23 19:56:39 -0700

    Make the bash script async

--- stream.js
+++ stream.js
@@ -1,11 +1,11 @@
 const shell = require('shelljs');
 
 function startStream() {
-  shell.exec('./start-pi-camera-secure.sh -lc -i 138.197.151.166 &', {silent: true});
+  shell.exec('./start-pi-camera-secure.sh -lc -i 138.197.151.166 &', {silent: true, async: true});
 }
 exports.startStream = startStream;
 
 function stopStream() {
-  shell.exec('./start-pi-camera-secure.sh &', {silent: true})
+  shell.exec('./start-pi-camera-secure.sh &', {silent: true, async: true})
 }
 exports.stopStream = stopStream;

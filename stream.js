e74f836fa5c606ca91d876d5120a57507bdb1db9
Sam Meech-Ward <sam@meech-ward.com>
2018-07-23 19:51:27 -0700

    Set silent to true

--- stream.js
+++ stream.js
@@ -1,11 +1,11 @@
 const shell = require('shelljs');
 
 function startStream() {
-  shell.exec('./start-pi-camera-secure.sh -lc &', {silent});
+  shell.exec('./start-pi-camera-secure.sh -lc &', {silent: true});
 }
 exports.startStream = startStream;
 
 function stopStream() {
-  shell.exec('./start-pi-camera-secure.sh &', {silent})
+  shell.exec('./start-pi-camera-secure.sh &', {silent: true})
 }
 exports.stopStream = stopStream;
